63d8cb9a0a27c45583da304b357eb4c8bf5194c7
Matheus <onlinestvs@gmail.com>
2025-04-10 18:36:25 -0300

    add login page template and update main application route

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.routers.crm import crm
 
 from app.middlewares.AuthMiddleware import AuthMiddleware
 
-app = FastAPI(title="FastAPI Example", description="A simple FastAPI example")
+app = FastAPI(title="FastAPI Example", description="A simple FastAPI example", redirect_slashes=False)
 app.mount('/static', StaticFiles(directory="./static"), name="static")
 templates = Jinja2Templates(directory="./templates")
 
