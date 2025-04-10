b3443d009016b717849e22e03541da9a81d6642e
Matheus <onlinestvs@gmail.com>
2025-04-10 16:38:23 -0300

    add login page template and update main application route

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from starlette.templating import Jinja2Templates
 import app.glob as glob
 
 from app.routers.auth import login
+from app.routers.crm import crm
 
 app = FastAPI(title="FastAPI Example", description="A simple FastAPI example")
 app.mount('/static', StaticFiles(directory="./static"), name="static")
@@ -15,3 +16,4 @@ templates.env.globals['v'] = os.environ.get('VERSION')
 glob.templates = templates
 
 app.include_router(login.router)
+app.include_router(crm.router)
