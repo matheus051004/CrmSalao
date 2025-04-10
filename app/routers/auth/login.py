8803673e60dfd260b408ba41bf5107f880fdf96c
Matheus <onlinestvs@gmail.com>
2025-04-10 14:40:10 -0300

    add login page template and update main application route

--- app/routers/auth/login.py
+++ app/routers/auth/login.py
@@ -1,6 +1,8 @@
+import os
+
 from fastapi import APIRouter
-from starlette.requests import Request
-from starlette.responses import HTMLResponse
+from fastapi import Request
+from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 
 router = APIRouter(
@@ -11,8 +13,39 @@ router = APIRouter(
 templates = Jinja2Templates(directory="./././templates")
 
 @router.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+async def login(request: Request):
     return templates.TemplateResponse("login.jinja", {
-        'v': "1.0.2",
+        'v': os.environ.get('VERSION'),
         'request': request,
     })
+
+@router.post("/", response_class=HTMLResponse)
+async def login_post(request: Request):
+    form = await request.form()
+    username = form.get("username")
+    password = form.get("password")
+
+    if username == os.environ["CRM_USER"] and password == os.environ["CRM_PASSWORD"]:
+        response = RedirectResponse(url="/crm", status_code=303)
+        response.set_cookie(
+            key="username",
+            value=username,
+            max_age=60 * 60 * 24 * 7,
+            httponly=False
+        )
+        response.set_cookie(
+            key="password",
+            value=password,
+            max_age=60 * 60 * 24 * 7,
+            httponly=False
+        )
+        return response
+    else:
+        response = templates.TemplateResponse("login.jinja", {
+            "v": os.environ.get("VERSION"),
+            "request": request,
+            "error": "Invalid username or password"
+        })
+        response.set_cookie("username", "false", max_age=60 * 60 * 24 * 7)
+        response.set_cookie("password", "false", max_age=60 * 60 * 24 * 7)
+        return response
