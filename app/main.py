ec350ac00c5a2fa96b6a3db9c4808799f5912e3c
Matheus <onlinestvs@gmail.com>
2025-04-10 12:38:53 -0300

    add login page template and update main application route

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-import os
-
 from fastapi import FastAPI
 from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from fastapi import Request
 
 app = FastAPI(title="FastAPI Example", description="A simple FastAPI example", version="1.0.0")
 
 templates = Jinja2Templates(directory="./templates")
+app.mount('/static', StaticFiles(directory="./static"), name="static")
 
 @app.get("/login", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("login.jinja", {"request": request})
