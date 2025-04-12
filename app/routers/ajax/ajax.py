11e477e0456bbc31720131c3b2a7b24ac9200e13
Matheus <onlinestvs@gmail.com>
2025-04-12 07:36:07 -0300

    add login page template and update main application route

--- app/routers/ajax/ajax.py
+++ app/routers/ajax/ajax.py
@@ -13,12 +13,24 @@ router = APIRouter(
 async def dashboard():
     db = SessionLocal()
     try:
-        print(get_monthly_clientes_count())
+        monthly_clientes_count = get_monthly_clientes_count()
         clientes_count = db.query(Cliente).count()
         clientes_mes_count = db.execute(text("SELECT COUNT(*) FROM clientes WHERE created_at >= NOW() - INTERVAL '1 month'")).scalar()
         return {
             'clientes_count': clientes_count,
             'clientes_mes_count': clientes_mes_count,
+            'clientes_jan' : monthly_clientes_count[0]['total'],
+            # 'clientes_feb' : monthly_clientes_count[1][1],
+            # 'clientes_mar' : monthly_clientes_count[2][1],
+            # 'clientes_apr' : monthly_clientes_count[3][1],
+            # 'clientes_may' : monthly_clientes_count[4][1],
+            # 'clientes_jun' : monthly_clientes_count[5][1],
+            # 'clientes_jul' : monthly_clientes_count[6][1],
+            # 'clientes_aug' : monthly_clientes_count[7][1],
+            # 'clientes_sep' : monthly_clientes_count[8][1],
+            # 'clientes_oct' : monthly_clientes_count[9][1],
+            # 'clientes_nov' : monthly_clientes_count[10][1],
+            # 'clientes_dec' : monthly_clientes_count[11][1],
         }
     finally:
         db.close()
