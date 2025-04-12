c7f8140eca3247e6cebdf53fc6c49beda3fcc765
Matheus <onlinestvs@gmail.com>
2025-04-12 08:12:51 -0300

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from app.models.cliente import Cliente
 from app.models.agendamentos import Agendamento
+from app.models.profissional import Profissional
 
 c = Cliente()
 a = Agendamento()
+p = Profissional()
