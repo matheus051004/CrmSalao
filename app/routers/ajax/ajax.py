c563a8c6a12e6ccd436518b92e09d5ce6d32365c
Matheus <onlinestvs@gmail.com>
2025-04-11 22:02:31 -0300

    add login page template and update main application route

--- app/routers/ajax/ajax.py
+++ app/routers/ajax/ajax.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from sqlalchemy import text
+
 from app.database import SessionLocal
 from app.models.cliente import Cliente
 
@@ -12,8 +14,10 @@ async def dashboard():
     db = SessionLocal()
     try:
         clientes_count = db.query(Cliente).count()
+        clientes_mes_count = db.query(text("SELECT COUNT(*) FROM clientes WHERE created_at >= NOW() - INTERVAL '1 month'")).scalar()
         return {
-            'clientes_count': clientes_count
+            'clientes_count': clientes_count,
+            'clientes_mes_count': clientes_mes_count,
         }
     finally:
         db.close()
