8157782e7d572e2202f612a8800aebde5e77e40d
Matheus <onlinestvs@gmail.com>
2025-04-12 08:10:07 -0300

    add login page template and update main application route

--- app/models/profissional.py
+++ app/models/profissional.py
@@ -3,14 +3,12 @@ from sqlalchemy import Column, Integer, String, DateTime, func
 from app.database import Base
 
 
-class Agendamento(Base):
+class Profissional(Base):
     __tablename__ = 'agendamentos'
     id = Column(Integer, primary_key=True, index=True)
-    cliente_id = Column(Integer, nullable=False)
-    profissional_id = Column(Integer, nullable=False)
-    start = Column(DateTime, nullable=False)
-    end = Column(DateTime, nullable=False)
-    title = Column(String)
-    description = Column(String, default="novo")
+    name = Column(String, nullable=False)
+    calendar_id = Column(String, nullable=True)
+    areas = Column(String, default='')
+    services = Column(String, default="masculino")
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
