fc65fb67b08c3a43e12aed7746bc40dd7dafd0b0
Matheus <onlinestvs@gmail.com>
2025-04-12 08:12:04 -0300

    add login page template and update main application route

--- app/models/profissional.py
+++ app/models/profissional.py
@@ -4,7 +4,7 @@ from app.database import Base
 
 
 class Profissional(Base):
-    __tablename__ = 'agendamentos'
+    __tablename__ = 'profissionais'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     calendar_id = Column(String, nullable=True)
