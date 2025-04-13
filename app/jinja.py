5ec7ac27f6637f4447002e9bd01159c5ca254d39
Matheus <onlinestvs@gmail.com>
2025-04-13 15:00:52 -0300

    ...

--- app/jinja.py
+++ app/jinja.py
@@ -10,6 +10,7 @@ def prepare_jinja(templates: Jinja2Templates):
     templates.env.globals['get_servico'] = get_servico
     templates.env.globals['get_cliente'] = get_cliente
     templates.env.globals['get_profissional'] = get_profissional
+    templates.env.globals['get_servicos_string'] = get_servicos_string
 
 
 def get_servico(service_id: int):
@@ -29,6 +30,21 @@ def get_servico(service_id: int):
             minutes=0,
             sexo='Indiferente'
         )
+    finally:
+        db.close()
+
+
+def get_servicos_string(service_ids: list[int]):
+    from app.database import SessionLocal
+    from app.models.servico import Servico
+
+    db = SessionLocal()
+    try:
+        servicos = db.query(Servico).filter(Servico.id.in_(service_ids)).all()
+        if servicos:
+            return ', '.join([servico.name for servico in servicos])
+
+        return 'Serviço não encontrado'
     finally:
         db.close()
 
