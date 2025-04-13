86175edfe477a762b28668209a2da005e164f36c
Matheus <onlinestvs@gmail.com>
2025-04-13 14:27:42 -0300

    ...

---
+++ app/jinja.py
@@ -0,0 +1,69 @@
+import os
+
+from fastapi.templating import Jinja2Templates
+
+
+def prepare_jinja(templates: Jinja2Templates):
+    templates.env.globals['v'] = os.environ.get('VERSION')
+    templates.env.globals['crm_name'] = os.environ.get('CRM_NAME')
+
+    templates.env.globals['get_servico'] = get_servico
+    templates.env.globals['get_cliente'] = get_cliente
+    templates.env.globals['get_profissional'] = get_profissional
+
+
+def get_servico(service_id: int):
+    from app.database import SessionLocal
+    from app.models.servico import Servico
+
+    db = SessionLocal()
+    try:
+        servico = db.query().filter(Servico.id == service_id).first()
+        if servico:
+            return servico
+
+        return Servico(
+            name='Serviço não encontrado',
+            price=0,
+            description='Serviço não encontrado',
+            minutes=0,
+            sexo='Indiferente'
+        )
+    finally:
+        db.close()
+
+
+def get_cliente(cliente_id: int):
+    from app.database import SessionLocal
+    from app.models.cliente import Cliente
+
+    db = SessionLocal()
+    try:
+        cliente = db.query().filter(Cliente.id == cliente_id).first()
+        if cliente:
+            return cliente
+
+        return Cliente(
+            name='Cliente não encontrado',
+            email='Cliente não encontrado',
+            phone='Cliente não encontrado',
+        )
+    finally:
+        db.close()
+
+
+def get_profissional(profissional_id: int):
+    from app.database import SessionLocal
+    from app.models.profissional import Profissional
+
+    db = SessionLocal()
+    try:
+        profissional = db.query().filter(Profissional.id == profissional_id).first()
+        if profissional:
+            return profissional
+
+        return Profissional(
+            name='Profissional não encontrado',
+        )
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import app.glob as glob
+from app.jinja import prepare_jinja
 
 from app.routers.auth import login
 from app.routers.crm import crm_dashboard
 from app.routers.crm import crm_clientes
+from app.routers.crm import crm_agendamentos
 
 from app.routers.ajax import ajax_dashboard
 from app.routers.ajax import ajax_evolution
@@ -21,13 +23,13 @@ app = FastAPI(title="FastAPI Example", description="A simple FastAPI example")
 app.mount('/static', StaticFiles(directory="./static"), name="static")
 templates = Jinja2Templates(directory="./templates")
 
-templates.env.globals['v'] = os.environ.get('VERSION')
-templates.env.globals['crm_name'] = os.environ.get('CRM_NAME')
 glob.templates = templates
+prepare_jinja(glob.templates)
 
 app.include_router(login.router)
 app.include_router(crm_dashboard.router)
 app.include_router(crm_clientes.router)
+app.include_router(crm_agendamentos.router)
 app.include_router(ajax_dashboard.router)
 app.include_router(ajax_evolution.router)
 app.include_router(ajax_clientes.router)
