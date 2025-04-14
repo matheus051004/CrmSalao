85f2293240e7f11fb37150c9804cb2016e0a2d3d
Matheus <onlinestvs@gmail.com>
2025-04-14 17:52:36 -0300

    ...

--- app/routers/n8n_api/__init__.py
+++ app/routers/n8n_api/__init__.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 
 n8n_api_router = APIRouter(
     prefix="/n8n-api",
-    tags=["n8n-api"],
-    include_in_schema=False,
+    include_in_schema=True,
 )
 
 from app.routers.n8n_api import agendamentos
