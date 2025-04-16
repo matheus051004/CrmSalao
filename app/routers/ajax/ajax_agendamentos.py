2dee08b2a6ccb2be521ce36395b080c892f86ef6
Matheus <onlinestvs@gmail.com>
2025-04-16 13:50:09 -0300

    ...

--- app/routers/ajax/ajax_agendamentos.py
+++ app/routers/ajax/ajax_agendamentos.py
@@ -51,6 +51,43 @@ async def concluir_agendamento(agendamento_confirm: AgendamentoComplete):
         db.close()
 
 
+@router.post('/cancelar', name='ajax-cancelar-agendamento')
+async def cancelar_agendamento(agendamento_confirm: AgendamentoComplete):
+    db = SessionLocal()
+    try:
+        agendamento = db.query(Agendamento).filter(Agendamento.id == agendamento_confirm.agendamento_id,
+                                                   Agendamento.status == 'agendado').first()
+        if not agendamento:
+            return response(False, 'Agendamento não encontrado ou já foi concluído')
+
+        profissional = db.query(Profissional).filter(Profissional.id == agendamento.profissional_id).first()
+        if not profissional:
+            raise response(False, 'Profissional não encontrado')
+
+        agendamento.status = 'cancelado'
+        db.commit()
+
+        # Google Calendar
+        gcm = GoogleCalendarManager(profissional.calendar_id, os.environ.get('GOOGLE_CREDENTIAL_JSON_B64'))
+        gcm.delete_event(agendamento.google_event_id)
+
+        # notificar
+        if agendamento_confirm.notify_cliente:
+            cliente = db.query(Cliente).filter(Cliente.id == agendamento.cliente_id).first()
+            if cliente:
+                msg_prepared = (agendamento_confirm.message
+                                .replace('{cliente_name}', cliente.name)
+                                .replace('{salao_name}', app_settings('salao_name')))
+
+                ev = Evolution(os.environ.get('EVOLUTION_API_URL'), os.environ.get('EVOLUTION_API_KEY'),
+                               os.environ.get('EVOLUTION_INSTANCE'))
+                ev.simple_text(cliente.phone, msg_prepared)
+
+        return response(True, 'Agendamento finalizado com sucesso')
+    finally:
+        db.close()
+
+
 def response(success=True, message="", data=None):
     return {
         "success": success,
