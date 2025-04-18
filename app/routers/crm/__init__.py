619ef148f2bc92f5077f140c9a14eca87136c52a
Matheus <onlinestvs@gmail.com>
2025-04-18 18:09:06 -0300

    ...

--- app/routers/crm/__init__.py
+++ app/routers/crm/__init__.py
@@ -10,8 +10,10 @@ from . import crm_agendamentos
 from . import crm_clientes
 from . import crm_dashboard
 from . import crm_servicos
+from . import crm_add_servico
 
 crm_router.include_router(crm_dashboard.router)
 crm_router.include_router(crm_clientes.router)
 crm_router.include_router(crm_agendamentos.router)
 crm_router.include_router(crm_servicos.router)
+crm_router.include_router(crm_add_servico.router)
