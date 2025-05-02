5e6dcb46d71a82f775b00d968c3778fb0891cc66
Matheus <onlinestvs@gmail.com>
2025-05-02 18:08:56 -0300

    ...

--- app/routers/ajax/ajax_agendamentos.py
+++ app/routers/ajax/ajax_agendamentos.py
@@ -88,6 +88,14 @@ async def cancelar_agendamento(agendamento_confirm: AgendamentoComplete):
         db.close()
 
 
+def replace_placeholders(text, salao_name = '', cliente_name = '', profissional_name = ''):
+    text = text.replace('{salao_name}', salao_name)
+    text = text.replace('{cliente_name}', cliente_name)
+    text = text.replace('{profissional_name}', profissional_name)
+    return text
+
+
+
 def response(success=True, message="", data=None):
     return {
         "success": success,
