91ef1071b41e824a8ce64741b2cbddfbdf5985ce
Matheus <onlinestvs@gmail.com>
2025-05-03 11:02:29 -0300

    ...

--- app/routers/crm/crm_configuracoes.py
+++ app/routers/crm/crm_configuracoes.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 import app.glob as g
 from app.database import SessionLocal
 from app.models.app_setting import AppSetting
-from app.models.pydantic.crm import Configs
+from app.models.pydantic.crm.Configs import Configs
 
 router = APIRouter(
     prefix="/configuracoes",
@@ -20,12 +20,12 @@ async def configuracoes(request: Request):
         'sidebar': 'configuracoes',
     })
 
-@router.post('/', name='configuracoes_postt')
+@router.post('/', name='configuracoes_post')
 async def configuracoes_post(config: Configs):
     s1 = AppSetting(key='salao_name', value=config.crm_name)
     s2 = AppSetting(key='msg_preference', value=config.msg_preference)
     s3 = AppSetting(key='msg_cancel', value=config.msg_cancel)
-    s4 = AppSetting(key='follow_up_minutes', value=config.follow_up_minutes)
+    s4 = AppSetting(key='follow_up_minutes', value=str(config.follow_up_minutes))
     s5 = AppSetting(key='msg_follow_up', value=config.msg_follow_up)
     db = SessionLocal()
     try:
